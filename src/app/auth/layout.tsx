93aa405dc6a24c2fef52dc45f10861d2f1957bc7
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2026-05-05 10:46:27 -0300

    style: auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,8 +2,8 @@ import type { LayoutProps } from "@/types";
 
 export default function AuthLayout({ children }: LayoutProps) {
   return (
-    <main className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-      <div className="w-full sm:max-w-md">{children}</div>
+    <main className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background md:p-10">
+      <div className="w-full md:max-w-md">{children}</div>
     </main>
   );
 }
